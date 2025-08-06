c4ffde59c998d664952a3f273f9098ebac7f94ff
Ayush Singh <ayushkrsingh4x@gmail.com>
2025-08-07 04:11:34 +0530

    📸 Added screenshots and updated README

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from scraper.court_scraper import fetch_court_data
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/fetch', methods=['POST'])
+def fetch_case():
+    case_type = request.form['case_type']
+    case_number = request.form['case_number']
+    filing_year = request.form['filing_year']
+    print(f"Form received: {case_type} {case_number} {filing_year}")
+    
+    data = fetch_court_data(case_type, case_number, filing_year)
+    return render_template('result.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
