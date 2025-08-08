189abd9c70b43e97d9e11136725bec342c37fd19
Ayush Kumar Singh <151716501+ayushxx17@users.noreply.github.com>
2025-08-09 01:43:09 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-arr = [[(1,2,3,4,5),(6,7,8,9,10),(11,12,13,14,15)]]
-print("Array : \n", arr)
-print(arr[0][0])  # use for accesing element in 01st rows.
-print(arr[1:2:])  #  "  "     "       "      in other rows.
