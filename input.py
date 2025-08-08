82f67ad2fec268fa8183de9679c0442a52d883f4
Ayush Kumar Singh <151716501+ayushxx17@users.noreply.github.com>
2025-08-09 01:53:36 +0530

    Delete input.py

--- input.py
+++
@@ -1,9 +0,0 @@
-name = input("name : ")
-age = int(input("age : "))
-price = float(input("price : "))
-
-print("my name is" ,name , "and I am", age , "year old") 
-print("i am", name, "and my age is", age )
-
-val = input("enter your name : ")
-print(type(val), val)
