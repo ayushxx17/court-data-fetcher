82b35939f01b3741fa4bfaf7e078d3297da7dfa6
Ayush Kumar Singh <151716501+ayushxx17@users.noreply.github.com>
2025-08-09 01:44:29 +0530

    Delete practice1.py

--- practice1.py
+++
@@ -1,9 +0,0 @@
-# WAP to input user's first name and print its length.
-name = input("enter your name : ")
-print(len(name))
-
-#WAP to sindthe occuerence of $ in  a string
-str = " Hii I am $ symbol $99.09"
-print(str.count("$"))
-
-
